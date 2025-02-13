80ecac7ea0e0c6b30981af5067b053061f4c13a2
Dan <145234317+finn1817@users.noreply.github.com>
2025-02-13 15:13:11 -0500

    more update install.py

--- install.py
+++ install.py
@@ -1,8 +1,9 @@
 import os
 
-print("Installing required Python packages...")
-os.system("pip install pandas openpyxl")
-print("Installation complete!")
+def install_packages():
+    print("Installing required Python packages...")
+    os.system("pip install pandas openpyxl pillow python-docx")
+    print("Installation complete!")
 
 if __name__ == "__main__":
     install_packages()
