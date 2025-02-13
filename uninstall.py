77d5e827a5d043f87b5393b916af30d2c6dcdc15
Dan <145234317+finn1817@users.noreply.github.com>
2025-02-13 15:13:38 -0500

    Updated uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -1,5 +1,9 @@
 import os
 
-print("Removing installed Python packages...")
-os.system("pip uninstall -y pandas openpyxl")
-print("Uninstall complete!")
+def uninstall_packages():
+    print("Removing installed Python packages...")
+    os.system("pip uninstall -y pandas openpyxl pillow python-docx")
+    print("Uninstall complete!")
+
+if __name__ == "__main__":
+    uninstall_packages()
